44fb6a19b32a583067d8deacc108daf1b17e2a89
Paulette Zaldivar Flores <paulettz@icloud.com>
2022-10-20 22:42:06 +0900

    refactor: tried to implement scrollspy
    -currently scrollspy not working at all

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#about-me'
+})
+
 $('.nav-link').click(function() {
   $('.nav-link').removeClass('active');
   $(this).addClass('active');
