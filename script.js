5937055dfa73c1b73aac82da53ed35f8e07e602d
Paulette Zaldivar Flores <paulettz@icloud.com>
2022-12-26 22:15:44 +0900

    refactor: implementing modal window for cards
    -showing same content currently

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ $('.nav-link').click(function() {
 $('.nav-link').removeClass('active');
 $(this).addClass('active');
 })
+
+// $('body').on('hidden.bs.modal', '.modal', function () {
+//   $(this).removeData('bs.modal');
+// });
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('this').removeData('bs.modal')
+})
