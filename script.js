b0b0c071d78dab494912c54a29d597b9cc46218a
Paulette Zaldivar Flores <paulettz@icloud.com>
2022-12-27 20:01:26 +0900

    style: updated css for modal window
    -implemented new modal windows with css only

--- script.js
+++ script.js
@@ -2,11 +2,3 @@ $('.nav-link').click(function() {
 $('.nav-link').removeClass('active');
 $(this).addClass('active');
 })
-
-// $('body').on('hidden.bs.modal', '.modal', function () {
-//   $(this).removeData('bs.modal');
-// });
-
-$('#myModal').on('shown.bs.modal', function () {
-  $('this').removeData('bs.modal')
-})
