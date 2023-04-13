74ede1ffe49db6f0d46a3f42cccf4d9215c35015
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-04-13 21:42:14 +0900

    refactor: added toggle for skills buttons and styled

--- src/script.js
+++ src/script.js
@@ -63,3 +63,48 @@
       }
     );
   });
+
+  // Tools Buttons
+
+  // Get buttons and pill list
+// Get buttons and pill list
+const frontendBtn = document.getElementById("frontend-btn");
+const backendBtn = document.getElementById("backend-btn");
+const otherToolsBtn = document.getElementById("other-tools-btn");
+const skillList = document.getElementById("skill-list");
+
+// Add click event listeners to buttons
+frontendBtn.addEventListener("click", () => showSkills("frontend"));
+backendBtn.addEventListener("click", () => showSkills("backend"));
+otherToolsBtn.addEventListener("click", () => showSkills("other-tools"));
+
+function showSkills(category) {
+  // Hide all pills
+  const pills = skillList.querySelectorAll(".pill");
+  pills.forEach(pill => pill.style.display = "none");
+
+  // Show pills for selected category
+  switch (category) {
+    case "frontend":
+      skillList.querySelector(".devicon-javascript-plain").parentNode.style.display = "block";
+      skillList.querySelector(".devicon-rails-plain").parentNode.style.display = "block";
+      skillList.querySelector(".devicon-react-original").parentNode.style.display = "block";
+      skillList.querySelector(".devicon-bootstrap-plain").parentNode.style.display = "block";
+      skillList.querySelector(".devicon-html5-plain").parentNode.style.display = "block";
+      skillList.querySelector(".devicon-css3-plain").parentNode.style.display = "block";
+
+      break;
+    case "backend":
+      skillList.querySelector(".devicon-ruby-plain").parentNode.style.display = "block";
+      skillList.querySelector(".devicon-postgresql-plain").parentNode.style.display = "block";
+      skillList.querySelector(".devicon-mysql-plain").parentNode.style.display = "block";
+      break;
+    case "other-tools":
+      skillList.querySelector(".devicon-webpack-plain").parentNode.style.display = "block";
+      skillList.querySelector(".devicon-heroku-plain").parentNode.style.display = "block";
+      skillList.querySelector(".devicon-figma-plain").parentNode.style.display = "block";
+      break;
+    default:
+      break;
+  }
+}
