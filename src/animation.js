af743203afc566a1e70cbeb997d0657fa3c0187e
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-08-01 14:13:34 +0900

    style: project card animation chnaged to fade up
    -better ui

--- src/animation.js
+++ src/animation.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   function animateProjectCards(entries, observer) {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("animate__animated", "animate__lightSpeedInLeft");
+        entry.target.classList.add("animate__animated", "animate__fadeInUp");
         entry.target.style.opacity = 1; // Set opacity to 1 when the card is in view
         observer.unobserve(entry.target); // Stop observing once animation is applied
       }
@@ -11,7 +11,7 @@ $(document).ready(function () {
   }
 
   // Create an Intersection Observer instance
-  const observer = new IntersectionObserver(animateProjectCards, { threshold: 0.2 });
+  const observer = new IntersectionObserver(animateProjectCards, { threshold: 0.1 });
 
   // Select all project cards and observe them
   const projectCards = document.querySelectorAll(".project__card");
